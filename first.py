b221421c65537c971124c95ef965a22e3524b0e1
AbhishekSharma911930 <asharma911930@gmail.com>
2023-05-19 14:00:36 +0530

    face_recognition

--- first.py
+++
@@ -1,9 +0,0 @@
-a=int(input('enter number'))
-b=int(input('enter number'))
-def abc():
-    print('good message')
-print(a+b)
-# dunder(__)
-if __name__=='__main__':
-    print("called from main")
-# this is used to create a module or for importing a file in python
--- first_program.py
+++
@@ -1,5 +0,0 @@
-import numpy
-numpy.array
-a=10
-b=20
-print(a*b)
